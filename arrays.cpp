58d26ea5148b975b02708e7c6d94ab659304ba8a
Wasif Ahmed <102247247+wasifns@users.noreply.github.com>
2024-04-23 23:58:45 +0530

    Update arrays.cpp

--- arrays.cpp
+++ arrays.cpp
@@ -1,1 +1,9 @@
 arrays
+Problem statement
+Given an array/list 'ARR' of integers and a position ‘M’. You have to reverse the array after that position.
+
+Example:
+
+We have an array ARR = {1, 2, 3, 4, 5, 6} and M = 3 , considering 0 
+based indexing so the subarray {5, 6} will be reversed and our 
+output array will be {1, 2, 3, 4, 6, 5}.
