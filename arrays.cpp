08c8e8d85d96b36cfed9de0596c6f820733ae681
Wasif Ahmed <102247247+wasifns@users.noreply.github.com>
2024-04-24 23:44:01 +0530

    Update arrays.cpp

--- arrays.cpp
+++ arrays.cpp
@@ -7,3 +7,23 @@ Example:
 We have an array ARR = {1, 2, 3, 4, 5, 6} and M = 3 , considering 0 
 based indexing so the subarray {5, 6} will be reversed and our 
 output array will be {1, 2, 3, 4, 6, 5}.
+  
+Sample Input 1:
+2
+6 3
+1 2 3 4 5 6
+5 2
+10 9 8 7 6
+Sample Output 1:
+1 2 3 4 6 5
+10 9 8 6 7
+Explanation 1:
+For the first test case, 
+Considering 0-based indexing we have M = 3 so the 
+subarray[M+1 … N-1] has to be reversed.
+Therefore the required output will be {1, 2, 3, 4, 6, 5}.
+
+For the second test case, 
+Considering 0-based indexing we have M = 2 so the 
+subarray[M+1 … N-1] has to be reversed.
+Therefore the required output will be {10, 9, 8, 6, 7}.
