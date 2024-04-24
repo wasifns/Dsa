840f348da62b91a1c53040ffb9a2e083f158f0b1
Wasif Ahmed <102247247+wasifns@users.noreply.github.com>
2024-04-24 23:45:36 +0530

    Update arrays.cpp

--- arrays.cpp
+++ arrays.cpp
@@ -27,3 +27,13 @@ For the second test case,
 Considering 0-based indexing we have M = 2 so the 
 subarray[M+1 … N-1] has to be reversed.
 Therefore the required output will be {10, 9, 8, 6, 7}.
+
+Sample Input 2:
+2
+7 3
+1 4 5 6 6 7 7 
+9 3
+10 4 5 2 3 6 1 3 6
+Sample Output 2:
+ 1 4 5 6 7 7 6
+ 10 4 5 2 6 3 1 6 3 
