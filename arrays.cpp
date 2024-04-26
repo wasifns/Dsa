471ed9575d76937a2c90c7b0575dc8b66eb38ab6
Wasif Ahmed <102247247+wasifns@users.noreply.github.com>
2024-04-26 23:48:57 +0530

    Update arrays.cpp

--- arrays.cpp
+++ arrays.cpp
@@ -108,3 +108,17 @@ Explanation:
 For i = 1, arr[1] <= arr[0]. So, 1 <= 3.
 For i = 2, arr[2] >= arr[1]. So, 2 >= 1.
 
+Given an integer array, print all distinct elements in an array. The given array may contain duplicates and the output should print every element only once. The given array is not sorted.
+
+Examples: 
+
+Input: arr[] = {12, 10, 9, 45, 2, 10, 10, 45}
+Output: 12, 10, 9, 45, 2
+
+Input: arr[] = {1, 2, 3, 4, 5}
+Output: 1, 2, 3, 4, 5
+
+
+Input: arr[] = {1, 1, 1, 1, 1}
+Output: 1
+
