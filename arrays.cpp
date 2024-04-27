3ced5bb36f34a44b9feb0e51e4f2783d4e13ce58
Wasif Ahmed <102247247+wasifns@users.noreply.github.com>
2024-04-27 23:21:47 +0530

    Update arrays.cpp

--- arrays.cpp
+++ arrays.cpp
@@ -129,4 +129,18 @@ Example:
 
 Let ‘N’ = 4,  ‘Arr’ be [1, 2, 5, 4] and ‘K’ = 3.  
 then the elements of this array in ascending order is [1, 2, 4, 5].  Clearly, the 3rd smallest and largest element of this array is 4 and 2 respectively.
-
+\
+Sample Input 1:
+2
+4 4
+5 6 7 2
+4 3
+1 2 5 4
+Sample Output 1:
+7 2 
+4 2
+Explanation of Sample Input 1:
+Test case 1:
+Here, ‘N’ = 4, ‘Arr’ = [5, 6, 7, 2] and ‘K’ = 3.
+Elements of the array in ascending order are [2, 5, 6, 7]
+Thus the 4rd smallest and 4rd largest elements of this array are 7 and 2 respectively.
