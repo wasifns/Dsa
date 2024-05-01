60db5e31c3cbe8857df165f0ebfe6d447e778655
Wasif Ahmed <102247247+wasifns@users.noreply.github.com>
2024-05-01 23:56:50 +0530

    Update arrays.cpp

--- arrays.cpp
+++ arrays.cpp
@@ -150,22 +150,5 @@ of elements (1, 2, 3, -2, 5) which
 is a contiguous subarray.
 Example 2:
 
-Input:
-N = 4
-Arr[] = {-1,-2,-3,-4}
-Output:
--1
-Explanation:
-Max subarray sum is -1 
-of element (-1)
-You don't need to read input or print anything. The task is to complete the function maxSubarraySum() which takes Arr[] and N as input parameters and returns the sum of subarray with maximum sum.
 
-
-Expected Time Complexity: O(N)
-Expected Auxiliary Space: O(1)
-
-
-Constraints:
-1 ≤ N ≤ 106
--107 ≤ A[i] ≤ 107
-
+  
