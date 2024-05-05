dd3f73c63a44260a2e0860bb9cae82c01c3e80f4
Wasif Ahmed <102247247+wasifns@users.noreply.github.com>
2024-05-05 23:23:33 +0530

    Update arrays.cpp

--- arrays.cpp
+++ arrays.cpp
@@ -116,17 +116,7 @@ You are given an array ‘Arr’ consisting of ‘N’ distinct integers and a p
 
 Example:
 
-Let ‘N’ = 4,  ‘Arr’ be [1, 2, 5, 4] and ‘K’ = 3.  
-then the elements of this array in ascending order is [1, 2, 4, 5].  Clearly, the 3rd smallest and largest element of this array is 4 and 2 respectively.
-\
-Sample Input 1:
-2
-4 4
-5 6 7 2
-4 3
-1 2 5 4
-
-  
+ 
 
 
   
