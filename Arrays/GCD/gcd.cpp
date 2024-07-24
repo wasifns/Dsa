af8a88ce566fd9b3d156bb137b42dd358d638441
Wasif Ahmed <102247247+wasifns@users.noreply.github.com>
2024-07-25 02:54:43 +0530

    Update gcd.cpp

--- Arrays/GCD/gcd.cpp
+++ Arrays/GCD/gcd.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 using namespace std;
 
+//BRUTE FORCE APPROACH
 int findGcd( int n, int m){
     int gcd = 1;
     for(int i = 2; i <= min(n,m); i++){
@@ -12,6 +13,29 @@ int findGcd( int n, int m){
     }
     return gcd;
 };
+
+//BETTER APPROACH
+#include <iostream>
+#include <algorithm>
+#include <cstdlib>
+#include <stdio.h>
+using namespace std;
+int findGcd(int n, int m){
+    int small, large;
+    int gcd = 0;
+    while (n != 0 && m !=0){
+        small = min(n,m);
+        large = max(n,m);
+        n = large - small;
+        m = small;
+        if( n == 0) gcd = m;
+        else gcd = n;
+    }
+    return gcd;
+}
+
+
+
 
 int main(){
     int n,m;
