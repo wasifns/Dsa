e847f9dc46d946d6d50cc59c1b3e1b2a8e9313b7
Wasif Ahmed <102247247+wasifns@users.noreply.github.com>
2024-08-04 23:43:40 +0530

    Update hashing.cpp

--- Arrays/Hashing/hashing.cpp
+++ Arrays/Hashing/hashing.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-//interget input
+//hash map for integer input
 int main(){
     int n;
     cin >> n;
@@ -8,13 +8,13 @@ int main(){
     for(int i = 0; i < n; i++){
         cin >> arr[i];
     }
-
+//pre-compute
 int hash[9] = {0};
 for(int i = 0; i < n; i++){
     hash[arr[i]] += 1;
 
 }
-
+//fetch
     int q;
     cin >> q;
     while(q--){
@@ -26,14 +26,15 @@ for(int i = 0; i < n; i++){
 
 }
 
-//String input
+//Hash map for String input
 string s;
 cin >> s;
 int hash[26] = {0};
 for(int i=0; i<s.size(); i++){
-    hash[s[i] - 'a']++; 
+    hash[s[i] - 'a']++; //pre-compute 
 }
 
+//fetch
 int q;
 cin >> q;
 while (q--){
