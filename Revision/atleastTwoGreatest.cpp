5df9a09e41468404370d6ebee100f9e50cc6f569
Wasif Ahmed <102247247+wasifns@users.noreply.github.com>
2024-08-25 23:39:41 +0530

    Add files via upload

---
+++ Revision/atleastTwoGreatest.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <limits.h>
+#include "vectorInput.h"
+using namespace std;
+
+void atleastTwoGreatest(std::vector<int> &vec){
+
+    int max1 = INT_MIN;
+    int max2 = INT_MIN;    
+    for(int i = 0; i < vec.size(); i++){
+        if(vec[i] > max1 ){
+    //updating max2 before max1 because, max1 will become second largest if found a bigger number,
+    //so by doing this, previous value of max1 will be updated to max2 first 
+            max2 = max1; 
+            max1 = vec[i];
+        }
+    //if above condition fails, check for max2 also max2 should be equal to max1
+        else if( vec[i] > max2 && vec[i] != max1){
+            max2 = vec[i];
+        }
+    }
+    
+    std::cout << "The two greatest elements: " << max1 << " " << max2 << endl;    
+}
+
+
+int main(){
+    int size;
+    std::cout << "Enter the size of array:\n";\
+    std::cin >> size;
+    std::vector<int> vec(size);
+    initialiseVector(vec);
+    atleastTwoGreatest(vec);
+}
+
+/*      ISSUES           SOLUTIONS
+    1-logic building: >using single for loop
+                      >simulteneously assigning values to max1 and max2 as the loop iteratres
+                      
+*/
