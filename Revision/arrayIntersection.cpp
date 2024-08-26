2cbd4fbf03925c5b7bf9b8be4fe271d258bf4648
Wasif Ahmed <102247247+wasifns@users.noreply.github.com>
2024-08-26 23:59:19 +0530

    Add files via upload

---
+++ Revision/arrayIntersection.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include "vectorInput.h"
+using namespace std;
+
+void arrayIntersection(vector<int> &vec, int size){
+    for (int i = 0 ; i < vec.size(); i++){
+        int element == vec[i];
+            for ( int j = 0; j < size; j++){
+                
+            }
+
+    }
+}
+
+int main(){
+    std::cout << "size?" ;
+    int size;
+    cin >> size;
+    vector<int> vec(size);
+    initialiseVector(vec);
+
+}
